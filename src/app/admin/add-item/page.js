e1e221344b72e9a45f03cf0d48824d2a7e5faf56
mustafakutlankale <mustafa.kale@metu.edu.tr>
2025-04-06 20:17:57 +0300

    Fix

--- src/app/admin/add-item/page.js
+++ src/app/admin/add-item/page.js
@@ -1,5 +1,7 @@
 "use client";
 export const dynamic = 'force-dynamic';
+export const dynamicParams = true;
+export const revalidate = 0;
 
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
